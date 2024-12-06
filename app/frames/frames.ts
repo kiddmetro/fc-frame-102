85224d4cc2e314579f606f1e704a135a7b6b130a
Igwe Clinton <igweclintonmatro@gmail.com>
2024-12-06 13:30:22 +0100

    update

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -1,6 +1,6 @@
 import { createFrames } from "frames.js/next";
 
 export const frames = createFrames({
-  baseUrl: process.env.BASE_URL || "http://localhost:3000",
+  baseUrl: process.env.BASE_URL,
   basePath: "/frames",
 });
